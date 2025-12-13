f650e453f1ffa6b56953d5e600993dee2e54968e
abrarasif11 <faasif929@gmail.com>
2025-12-13 10:23:13 +0600

    Material UI Integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { Button } from "@mui/material";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h2>Home Page</h2>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 };
