5d3fffbc580f4a8a7786c3fe882266331dc9f820
abrarasif11 <faasif929@gmail.com>
2025-12-13 12:22:46 +0600

    Create Footer

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,18 +1,86 @@
 "use client";
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <Container>
-      <Stack py={2} direction="row" justifyContent="space-between">
-        <Typography variant="h5" component="h1" fontWeight={600}>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography
+          variant="h4"
+          component={Link}
+          href="/"
+          fontWeight={600}
+          sx={{
+            textDecoration: "none",
+            color: "inherit",
+            "&:hover": {
+              textDecoration: "none",
+            },
+            "&:focus": {
+              textDecoration: "none",
+            },
+            "&:active": {
+              textDecoration: "none",
+            },
+          }}
+        >
           P
           <Box component="span" color="primary.main">
             H
           </Box>{" "}
           Health Care
         </Typography>
+        <Stack direction="row" justifyContent="space-between" gap={4}>
+          <Typography
+            component={Link}
+            href="/consultation"
+            sx={{
+              textDecoration: "none",
+
+              "&:hover, &:active, &:focus, &:visited": {
+                textDecoration: "none",
+              },
+            }}
+          >
+            Consultation
+          </Typography>
+
+          <Typography
+            component={Link}
+            href="/health-care"
+            sx={{
+              textDecoration: "none",
+
+              "&:hover, &:active, &:focus, &:visited": {
+                textDecoration: "none",
+              },
+            }}
+          >
+            Health Plans
+          </Typography>
+
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+        <Button
+          component={Link}
+          href="/login"
+          sx={{
+            "&:active": {
+              textDecoration: "none",
+            },
+          }}
+        >
+          Login
+        </Button>
       </Stack>
     </Container>
   );
