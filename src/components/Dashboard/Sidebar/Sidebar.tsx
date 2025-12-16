29396f2c95c14d202ea77ba57e997e17fb50ce7a
abrarasif11 <faasif929@gmail.com>
2025-12-16 14:26:16 +0600

    Creating a Sidebar for Menu Items

---
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -0,0 +1,81 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import Image from "next/image";
+import logo from "../../../assets/svgs/logo.svg";
+import Link from "next/link";
+
+const Sidebar = () => {
+  const drawer = (
+    <div>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <Box>
+      <Stack
+        component={Link}
+        href="/"
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        gap={1}
+        sx={{
+          py: 1,
+          mt: 1,
+          textDecoration: "none",
+          color: "inherit",
+          "&:hover": {
+            textDecoration: "none",
+          },
+        }}
+      >
+        <Image src={logo} width={40} height={40} alt="logo" />
+
+        <Typography variant="h6" component="h1" sx={{ cursor: "pointer" }}>
+          PH Health Care
+        </Typography>
+      </Stack>
+
+      {drawer}
+    </Box>
+  );
+};
+
+export default Sidebar;
