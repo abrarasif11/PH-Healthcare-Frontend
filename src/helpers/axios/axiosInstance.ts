ee8573ba3a127c1db39b31bd8106e52b83f68cdd
abrarasif11 <faasif929@gmail.com>
2025-12-17 18:24:11 +0600

    Deep Dive Into Axios Instance

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -1,6 +1,37 @@
+import { authKey } from "@/contants/authKey";
+import { getFromLocalStorage } from "@/utils/local-storage";
 import axios from "axios";
 
 const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 6000;
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+
+    const accessToken = getFromLocalStorage(authKey)
+    if(accessToken){
+        config.headers.Authorization = accessToken
+    }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+  { synchronous: true, runWhen: () => /* This function returns true */}
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(function onFulfilled(response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function onRejected(error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
+
 
 export { instance };
