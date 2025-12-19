7a78b8fc39d258bd3af3e1ef538682e09a1c7f67
abrarasif11 <faasif929@gmail.com>
2025-12-19 18:56:43 +0600

    use admin doctor page

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import React from "react";
 
 const DoctorsPage = () => {
   return (
-    <div>
-      <h1>DoctorsPage</h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Create New Doctor</Button>
+        <TextField size="small" placeholder="Search Doctor" />
+      </Stack>
+    </Box>
   );
 };
 
