3dd83ea09e490babda76e8f0248ab199d809b36b
abrarasif11 <faasif929@gmail.com>
2025-12-19 18:22:00 +0600

    Displaying Specialty Data Using Data Grid Table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,43 +1,56 @@
 "use client";
-import { Box, Button, Stack, TextField } from "@mui/material";
-import React, { useState } from "react";
-import SpecialistModal from "./components/SpecialtyModel";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
+
+import { useState } from "react";
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import SpecialtyModel from "./components/SpecialtyModel";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { data, isLoading } = useGetAllSpecialtiesQuery({});
-  // console.log(data);
 
+  // console.log(data);
   const columns: GridColDef[] = [
-    { field: "Title", headerName: "Title", width: 70 },
+    { field: "title", headerName: "Title", width: 400 },
+    // {
+    //   field: "icon",
+    //   headerName: "Icon",
+    //   flex: 1,
+    //   renderCell: ({ row }) => {
+    //     return (
+    //       <Box>
+    //         <Image src={row.icon} width={30} height={30} alt="icon" />
+    //       </Box>
+    //     );
+    //   },
+    // },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return <></>;
+      },
+    },
   ];
 
   return (
     <Box>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignContent="center"
-      >
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Button onClick={() => setIsModalOpen(true)}>Create Specialty</Button>
-        <SpecialistModal open={isModalOpen} setOpen={setIsModalOpen} />
-        <TextField size="small" placeholder="Search Specialties" />
+        <SpecialtyModel open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Specialist" />
       </Stack>
       {!isLoading ? (
-        <Box>
-          <DataGrid
-            rows={data}
-            columns={columns}
-            initialState={{ pagination: {} }}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-            sx={{ border: 0 }}
-          />
+        <Box my={2}>
+          <DataGrid rows={data} columns={columns} hideFooter={true} />
         </Box>
       ) : (
-        <h1>Loading...</h1>
+        <h1>Loading.....</h1>
       )}
     </Box>
   );
