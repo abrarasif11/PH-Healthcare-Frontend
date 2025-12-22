ceefca0be3c2ef08ee74051a8022ac8bcd92ac3d
abrarasif11 <faasif929@gmail.com>
2025-12-22 20:03:30 +0600

    Dynamic Doctor Update Page

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
-
-const page = () => {
-  return <div></div>;
+type TParams = {
+  params: {
+    doctorId: string;
+  };
+};
+const DoctorUpdatePage = ({ params }: TParams) => {
+  console.log(params?.doctorId);
+  return (
+    <div>
+      <h4>Update Doctor</h4>
+    </div>
+  );
 };
 
-export default page;
+export default DoctorUpdatePage;
