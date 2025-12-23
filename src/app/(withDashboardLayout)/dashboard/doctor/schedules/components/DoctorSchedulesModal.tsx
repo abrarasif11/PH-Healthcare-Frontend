844755826140afac28a4edc7f97fe378e250ae01
abrarasif11 <faasif929@gmail.com>
2025-12-23 20:43:08 +0600

    MultipleSelectChip created

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorSchedulesModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorSchedulesModal.tsx
@@ -4,6 +4,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import dayjs, { Dayjs } from "dayjs";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
 
 type TProps = {
   open: boolean;
@@ -11,6 +12,22 @@ type TProps = {
 };
 const DoctorSchedulesModal = ({ open, setOpen }: TProps) => {
   const [selectedDate, setSelectedDate] = useState(dayjs().toISOString());
+  const query: Record<string, any> = {};
+  if (!!selectedDate) {
+    query["startDate"] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+    query["endDate"] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+  const { data, isLoading } = useGetAllSchedulesQuery(query);
+  const schedules = data?.schedules;
+  console.log(schedules);
   return (
     <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
