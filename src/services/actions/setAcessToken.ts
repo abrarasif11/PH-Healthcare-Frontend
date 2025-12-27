ea59d23a8f1409426e011d9d22c3b8e7032c0fab
abrarasif11 <faasif929@gmail.com>
2025-12-27 15:05:44 +0600

    create change pass

--- src/services/actions/setAcessToken.ts
+++ src/services/actions/setAcessToken.ts
@@ -7,12 +7,12 @@ import { redirect } from "next/navigation";
 
 const setAccessToken = (token: string, option?: any) => {
   cookies().set(authKey, token);
-  //   if (option && option.passwordChangeRequired) {
-  //     redirect("/dashboard/change-password");
-  //   }
-  //   if (option && !option.passwordChangeRequired && option.redirect) {
-  //     redirect(option.redirect);
-  //   }
+  if (option && option.passwordChangeRequired) {
+    redirect("/dashboard/change-password");
+  }
+  if (option && !option.passwordChangeRequired && option.redirect) {
+    redirect(option.redirect);
+  }
   redirect("/dashboard");
 };
 
